22221fca300be375b99b115dbb4bbeacbd234f09
h13 <1491918308@qq.com>
2020-08-12 16:26:28 +0800

    0.1

---
+++ BrowerLawncher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BrowerLawncher
+{
+    class StartBrowser
+    {
+        private String path;
+        
+
+        public StartBrowser(String path)
+        {
+            this.path = path;
+        }
+
+        public void Start()
+        {
+            //准备cmd
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+            p.Start();//启动程序
+
+            //读取配置
+            IEnumerable<String> conf = File.ReadLines(path + ".conf");
+
+            if (!File.Exists(conf.ElementAt<String>(0)))
+            {
+                //如果不存在对应浏览器,则提示后退出
+                MessageBox.Show("Target Browser: \"" + conf.ElementAt<String>(0) + "\" Not exist." , "Brower Lawncher:");
+                System.Environment.Exit(0);
+            }
+
+            Console.Out.WriteLine(conf.ElementAt<String>(0));
+            //向cmd窗口发送输入信息
+            String command = "";
+            for(int index = 0;index < conf.ToArray().Length;index++)
+            {
+                if(index != 0)
+                {
+                    command = command + " " + conf.ElementAt<String>(index);
+                }
+                else
+                {
+                    command = "\"" + conf.ElementAt<String>(0) + "\"";
+                }
+            }
+
+            command += "&exit";
+
+            Console.Out.WriteLine(command);
+            p.StandardInput.WriteLine(command);
+
+            //开完等一下再跑
+            System.Threading.Thread.Sleep(1000);
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace BrowerLawncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            System.Resources.ResourceManager resources =
+                new System.Resources.ResourceManager("myResource", System.Reflection.Assembly.GetExecutingAssembly());
+            NotifyIcon ni = new NotifyIcon();
+
+            ni.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            ni.BalloonTipText = "test!";
+            ni.BalloonTipTitle = "test.";
+            //ni.ContextMenuStrip = contextMenu;  
+            //ni.Icon = ((System.Drawing.Icon)(resources.GetObject("ni.Icon")));
+            ni.Text = "Test";
+            ni.Visible = true;
+            ni.MouseClick += delegate (object sender, MouseEventArgs e)
+            {
+                ni.ShowBalloonTip(0);
+            };
+
+            new Thread(new StartBrowser(Application.ExecutablePath).Start).Start();
+
+            Application.Run();
+        }
+    }
+}
